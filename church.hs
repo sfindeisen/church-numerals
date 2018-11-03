076375bdd6c97dc74944094afe8d69c9ce0ce407
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2018-11-03 21:50:03 +0100

    implement plus

--- church.hs
+++ church.hs
@@ -9,6 +9,9 @@ church n = \f -> \x -> f (church (n-1) f x)
 unchurch :: Church Integer -> Integer
 unchurch cn = cn (+ 1) 0
 
+plus :: Church Integer -> Church Integer -> Church Integer
+plus m n f x = m f (n f x)
+
 ---------------------------------------
 ---- QuickCheck
 ---------------------------------------
@@ -16,15 +19,20 @@ unchurch cn = cn (+ 1) 0
 gen100 :: Gen Integer
 gen100 = choose (0, 100)
 
-prop_roundtrip x = x == unchurch (church x)
+prop_roundtrip x =
+    x == unchurch (church x)
+
+prop_plus x y =
+    (x+y) == unchurch (plus (church x) (church y))
 
 ---------------------------------------
 ---- main
 ---------------------------------------
 
---check = verboseCheck
+-- check = verboseCheck
 check = quickCheck
 
 main :: IO ()
 main = do
     check $ forAll gen100 prop_roundtrip
+    check $ forAll gen100 (\x -> forAll gen100 (prop_plus x))
